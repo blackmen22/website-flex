cec561bf177c7e97712aa0c87e1d21a3967fb8e1
Black men22 <agnieszka.izuk.01@gmail.com>
2020-02-18 17:34:42 +0100

    ??bug

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-
-$(function() {
-  
-  // contact form animations
-  $('#contact').click(function() {
-    $('#contactForm').fadeToggle();
-  })
-  $(document).mouseup(function (e) {
-    var container = $("#contactForm");
-
-    if (!container.is(e.target) // if the target of the click isn't the container...
-        && container.has(e.target).length === 0) // ... nor a descendant of the container
-    {
-        container.fadeOut();
-    }
-  });
-  
-});
-=======
 function validateForm(){
   var nameRegex = /^[A-Za-z]+$/;
   var fnameVal = $('#fname').val();
@@ -46,4 +26,3 @@ function validateForm(){
   }
   
 }
->>>>>>> add contact form not responsible
